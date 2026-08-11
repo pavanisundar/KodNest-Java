445abe8b2b5346bdae716c23cd1d5cdee1a612ee
Pavani <pavanisundar25@gmail.com>
2026-08-11 11:27:45 +0530

    Employee

---
+++ track/M02/T01/EmpApplication.java
@@ -0,0 +1,30 @@
+
+class Employee {
+
+    int id;
+    String name;
+
+    void work() {
+        System.out.println("working");
+    }
+}
+
+class EmpApplication {
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        e1.id = 11;
+        e1.name = "Uma";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        e1.work();
+
+        Employee e2 = new Employee();
+        e2.id = 13;
+        e2.name = "Raja";
+        System.out.println(e2.id);
+        System.out.println(e2.name);
+
+    }
+
+}
