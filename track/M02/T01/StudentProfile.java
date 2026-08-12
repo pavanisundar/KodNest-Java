8c96c9d24d33f1c18cd9d5099e6e2dce66317a37
Pavani <pavanisundar25@gmail.com>
2026-08-12 10:10:38 +0530

    StudentProfile

---
+++ track/M02/T01/StudentProfile.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class StudentProfile {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.next();
+        int projects = sc.nextInt();
+        double assessments = sc.nextDouble();
+
+        System.out.println("Learner: " + name);
+        System.out.println("Problems solved: " + projects);
+        System.out.println("Assessment: " + assessments);
+    }
+}
