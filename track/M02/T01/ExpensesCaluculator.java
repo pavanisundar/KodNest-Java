d4b7aac4fd29c2e4087a0310e5e25d9dc7ff921f
Pavani <pavanisundar25@gmail.com>
2026-08-12 10:00:19 +0530

    ExpensesCaluclator

---
+++ track/M02/T01/ExpensesCaluculator.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class ExpensesCaluculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double monthlyIncome = scanner.nextDouble();
+        double rentExpense = scanner.nextDouble();
+        double foodExpense = scanner.nextDouble();
+        double travelExpense = scanner.nextDouble();
+
+        double totalExpense = rentExpense + foodExpense + travelExpense;
+        double remainingAmount = monthlyIncome - totalExpense;
+
+        System.out.println("Total expense: " + totalExpense);
+        System.out.println("Remaining: " + remainingAmount);
+
+        if (remainingAmount >= 0) {
+            System.out.println("Status: Within budget");
+        } else {
+            System.out.println("Status: Over budget");
+        }
+
+        scanner.close();
+    }
+}
