cbaadf5d6da8e133d9614d188bcd3424d72a443f
Pavani <pavanisundar25@gmail.com>
2026-08-17 12:08:40 +0530

    array1

---
+++ track/M02/T01/Array/pgm2.java
@@ -0,0 +1,12 @@
+
+public class pgm2 {
+
+    public static void main(String[] args) {
+        int a[] = {11, 24, 34, 49, 60};
+
+        // Using a for loop to print all elements
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
