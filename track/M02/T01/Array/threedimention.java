9754c96f785ac54d2342bef32eec897e84580e54
Pavani <pavanisundar25@gmail.com>
2026-08-31 15:46:06 +0530

    SCD

--- track/M02/T01/Array/threedimention.java
+++ track/M02/T01/Array/threedimention.java
@@ -1,7 +1,9 @@
 
-import java.util.Scanner;
+import static java.lang.System.out;
 
-class Threedimention {
+import java.util.scanner;
+
+public class Threedimention1 {
 
     public static void main(String[] args) {
         int a[][][] = new int[3][3][5];
@@ -9,21 +11,34 @@ class Threedimention {
         System.out.println("Enter the Array Elements: ");
 
         for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                for (int k = 0; k < a[i][j].length; k++) {
+            for (int j = 0; j < a[i].length - 1; j++) {
+                for (int k = 0; k < a[i][j].length - 1; k++) {
                     a[i][j][k] = scan.nextInt();
+
                 }
             }
+        }
+    }
+
+    System.out.print (
+            
+
+    " Array Elements are: ");
+     (int i
+    
+    = 0; i< a.length ;
+    i
+
+    
+        ++) {
+                for (int j = 0; j < a[i].length - 1 j++;
+        
+            ) {
+                for (int k = 0; k < a[i][j].length - 1; k++) {
+                System.out.print(" " + a[i][j][k] + " ");
+            }
+            System.out.println();
         }
 
-        System.out.println("Array Elements are:");
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                for (int k = 0; k < a[i][j].length; k++) {
-                    System.out.print(" " + a[i][j][k] + " ");
-                }
-                System.out.println();
-            }
-        }
     }
 }
