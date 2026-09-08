0179e9ba7a1811335107b609d6a082a66a4643c0
Pavani <pavanisundar25@gmail.com>
2026-09-08 16:52:57 +0530

    reference

---
+++ track/M02/T01/Array/Strings/pgm2.java
@@ -0,0 +1,17 @@
+class pgm2 {
+    public static void main(String[] args) {
+        String s1 = "java";
+        String s2 = "java";
+
+        if (s1 == s2) {
+            System.out.println("Reference are equal");
+        } else {
+            System.out.println("Reference are not Equal");
+        }
+        if (s1.equalsIgnoreCase(s2)){
+            System.out.println(" Strings are Equal");
+        }else{
+            System.out.println(" String are Not Equal");
+        }
+    }
+}
