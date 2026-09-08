42356a168bf413527781efb8ec05904f61cdee6b
Pavani <pavanisundar25@gmail.com>
2026-09-08 17:47:05 +0530

    pgm5

---
+++ pgm5.java
@@ -0,0 +1,10 @@
+
+public class pgm5 {
+
+    public static void main(String[] args) {
+        String s1 = " java ";
+        System.out.println(s1);
+        System.out.println(s1.trim());
+
+    }
+}
